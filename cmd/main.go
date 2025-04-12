66e695ce6e5b7309460f56c32f1e612dea6d09cc
MarcosAndreLS <marcosofc04@gmail.com>
2025-04-12 11:06:12 -0300

    Rota: Buscar produtos pt 1

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,25 @@
 package main
 
-import(
+import (
+	"github.com/MarcosAndreLS/go-rest-api/controller"
+	"github.com/MarcosAndreLS/go-rest-api/usecase"
 	"github.com/gin-gonic/gin"
 )
 
 func main(){
 	
 	server := gin.Default()
+	ProductUseCase := usecase.NewProductUseCase()
+	//camada de controllers
+	productController := controller.NewProductController(ProductUseCase)
 
 	server.GET("/ping", func(ctx *gin.Context){
 		ctx.JSON(200, gin.H{
 			"message" : "pong",
 		})
 	})
+
+	server.GET("/products", productController.GetProducts)
+
 	server.Run(":8000")
 }

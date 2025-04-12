a8a5b1f0b871c022db766de5413c84db45e335a8
MarcosAndreLS <marcosofc04@gmail.com>
2025-04-12 14:28:52 -0300

    Rota: Buscar produtos pt 2

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/MarcosAndreLS/go-rest-api/controller"
 	"github.com/MarcosAndreLS/go-rest-api/db"
+	"github.com/MarcosAndreLS/go-rest-api/repository"
 	"github.com/MarcosAndreLS/go-rest-api/usecase"
 	"github.com/gin-gonic/gin"
 )
@@ -14,7 +15,10 @@ func main(){
 	if (err != nil){
 		panic(err)
 	}
-	ProductUseCase := usecase.NewProductUseCase()
+	//camda de repository
+	ProductRepository := repository.NewProductRepository(dbConnection)
+	// camada de usecase
+	ProductUseCase := usecase.NewProductUseCase(ProductRepository)
 	//camada de controllers
 	productController := controller.NewProductController(ProductUseCase)
 
